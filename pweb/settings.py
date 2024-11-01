4bfe8ae43e6949126135f164542b9a365c2fb344
João Victor <jvictorsantos852@gmail.com>
2024-11-01 02:48:13 -0300

    chore(wsgi.py): assign get_wsgi_application() to 'app' variable for clarity and consistency

--- pweb/settings.py
+++ pweb/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("VERCEL_ENV") != "production"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
